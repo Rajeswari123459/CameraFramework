d8fcfc9cfa07139382739be450c8747993ae0612
Rajeswari <rajeswari.natesan@bank-genie.com>
2023-07-20 10:05:07 +0530

    Finished our Camera Framework

---
+++ CameraFramework/CameraFramework/CameraFramework.h
@@ -0,0 +1,18 @@
+//
+//  CameraFramework.h
+//  CameraFramework
+//
+//  Created by Bank-Genie PC3 on 19/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CameraFramework.
+FOUNDATION_EXPORT double CameraFrameworkVersionNumber;
+
+//! Project version string for CameraFramework.
+FOUNDATION_EXPORT const unsigned char CameraFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraFramework/PublicHeader.h>
+
+
