e2da0849463bff7d906886eb16037b0594337bad
Rajeswari <rajeswari.natesan@bank-genie.com>
2023-07-24 19:46:52 +0530

    Added framework source and podspec

---
+++ checksc/Pods/Target Support Files/Pods-checksc-checkscUITests/Pods-checksc-checkscUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_checksc_checkscUITests : NSObject
+@end
+@implementation PodsDummy_Pods_checksc_checkscUITests
+@end
---
+++ checksc/Pods/Target Support Files/Pods-checkscTests/Pods-checkscTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_checkscTests : NSObject
+@end
+@implementation PodsDummy_Pods_checkscTests
+@end
